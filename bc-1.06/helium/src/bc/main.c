5374af8f70c8d0b2aa72e2c6839152f26e589d3e
Hebi Li <lihebi.com@gmail.com>
2016-04-29 00:19:08 -0500

    give up bc

--- bc-1.06/helium/src/bc/main.c
+++ bc-1.06/helium/src/bc/main.c
@@ -57,19 +57,6 @@ static struct option long_options[] =
 };
 
 
-void
-usage (char *progname)
-{
-  printf ("usage: %s [options] [file ...]\n%s%s%s%s%s%s%s", progname,
-          "  -h  --help         print this usage and exit\n",
-	  "  -i  --interactive  force interactive mode\n",
-	  "  -l  --mathlib      use the predefine math routnes\n",
-	  "  -q  --quiet        don't print initial banner\n",
-	  "  -s  --standard     non-standard bc constructs are errors\n",
-	  "  -w  --warn         warn about non-standard bc constructs\n",
-	  "  -v  --version      print version information and exit\n");
-}
-
 
 void
 parse_args (argc, argv)
@@ -91,46 +78,6 @@ parse_args (argc, argv)
       if (optch == EOF)  /* End of arguments. */
 	break;
 
-      switch (optch)
-	{
-	case 'c':  /* compile only */
-	  compile_only = TRUE;
-	  break;
-
-	case 'h':  /* help */
-	  usage(argv[0]);
-	  exit (0);
-	  break;
-
-	case 'i':  /* force interactive */
-	  interactive = TRUE;
-	  break;
-
-	case 'l':  /* math lib */
-	  use_math = TRUE;
-	  break;
-
-	case 'q':  /* quiet mode */
-	  quiet = TRUE;
-	  break;
-
-	case 's':  /* Non standard features give errors. */
-	  std_only = TRUE;
-	  break;
-
-	case 'v':  /* Print the version. */
-	  show_bc_version ();
-	  exit (0);
-	  break;
-
-	case 'w':  /* Non standard features give warnings. */
-	  warn_not_std = TRUE;
-	  break;
-
-	default:
-	  usage(argv[0]);
-	  exit (1);
-	}
     }
 
   /* Add file names to a list of files to process. */
@@ -156,57 +103,10 @@ main (argc, argv)
      char *argv[];
 {
   char *env_value;
-  char *env_argv[30];
-  int   env_argc;
  
-  /* Initialize many variables. */
-  compile_only = FALSE;
-  use_math = FALSE;
-  warn_not_std = FALSE;
-  std_only = FALSE;
-  if (isatty(0) && isatty(1)) 
-    interactive = TRUE;
-  else
-    interactive = FALSE;
-  quiet = FALSE;
-  file_names = NULL;
-
-#ifdef HAVE_SETVBUF
-  /* attempt to simplify interaction with applications such as emacs */
-  (void) setvbuf(stdout, NULL, _IOLBF, 0);
-#endif
-
-  /* Environment arguments. */
-  env_value = getenv ("BC_ENV_ARGS");
-  if (env_value != NULL)
-    {
-      env_argc = 1;
-      env_argv[0] = "BC_ENV_ARGS";
-      while (*env_value != 0)
-	{
-	  if (*env_value != ' ')
-	    {
-	      env_argv[env_argc++] = env_value;
-	      while (*env_value != ' ' && *env_value != 0)
-		env_value++;
-	      if (*env_value != 0)
-		{
-		  *env_value = 0;
-		  env_value++;
-		}
-	    }
-	  else
-	    env_value++;
-	}
-      parse_args (env_argc, env_argv);
-    }
-
   /* Command line arguments. */
   parse_args (argc, argv);
 
-  /* Other environment processing. */
-  if (getenv ("POSIXLY_CORRECT") != NULL)
-    std_only = TRUE;
 
   env_value = getenv ("BC_LINE_LENGTH");
   if (env_value != NULL)
@@ -222,10 +122,6 @@ main (argc, argv)
   init_storage();
   init_load();
 
-  /* Set up interrupts to print a message. */
-  if (interactive)
-    signal (SIGINT, use_quit);
-
   /* Initialize the front end. */
   init_tree();
   init_gen ();
@@ -234,34 +130,9 @@ main (argc, argv)
   if (!open_new_file ())
     exit (1);
 
-#if defined(LIBEDIT)
-  if (interactive) {
-    /* Enable libedit support. */
-    edit = el_init ("bc", stdin, stdout, stderr);
-    hist = history_init();
-    el_set (edit, EL_EDITOR, "emacs");
-    el_set (edit, EL_HIST, history, hist);
-    el_set (edit, EL_PROMPT, null_prompt);
-    el_source (edit, NULL);
-    history (hist, &histev, H_SETSIZE, INT_MAX);
-  }
-#endif
-
-#if defined(READLINE)
-  if (interactive) {
-    /* Readline support.  Set both application name and input file. */
-    rl_readline_name = "bc";
-    rl_instream = stdin;
-    using_history ();
-  }
-#endif
 
   /* Do the parse. */
   yyparse ();
-
-  /* End the compile only output with a newline. */
-  if (compile_only)
-    printf ("\n");
 
 
   exit (0);
@@ -287,29 +158,6 @@ open_new_file ()
 
   /* Check to see if we are done. */
   if (is_std_in) return (FALSE);
-
-  /* Open the other files. */
-  if (use_math && first_file)
-    {
-      /* Load the code from a precompiled version of the math libarary. */
-      extern char *libmath[];
-      char **mstr;
-      char tmp;
-      /* These MUST be in the order of first mention of each function.
-	 That is why "a" comes before "c" even though "a" is defined after
-	 after "c".  "a" is used in "s"! */
-      tmp = lookup ("e", FUNCT);
-      tmp = lookup ("l", FUNCT);
-      tmp = lookup ("s", FUNCT);
-      tmp = lookup ("a", FUNCT);
-      tmp = lookup ("c", FUNCT);
-      tmp = lookup ("j", FUNCT);
-      mstr = libmath;
-      while (*mstr) {
-           load_code (*mstr);
-	   mstr++;
-      }
-    }
   
   /* One of the argv values. */
   if (file_names != NULL)
@@ -324,13 +172,7 @@ open_new_file ()
 	  free (temp);
 	  return TRUE;
 	}
-      fprintf (stderr, "File %s is unavailable.\n", file_names->name);
-      exit (1);
     }
-  
-  /* If we fall through to here, we should return stdin. */
-  new_yy_file (stdin);
-  is_std_in = TRUE;
   return TRUE;
 }
 
