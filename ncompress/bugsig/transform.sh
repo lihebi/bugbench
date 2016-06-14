059a968b7ea27bb810aaed44a454b8cf2641151a
Hebi Li <lihebi.com@gmail.com>
2016-06-14 12:00:44 -0500

    fix linux sed -i

--- ncompress/bugsig/transform.sh
+++ ncompress/bugsig/transform.sh
@@ -9,10 +9,10 @@ sed -E -e "460,$ s/REG[0-9]{1,2}//g" compress42.c.orig > compress42.bugsig.c
 # add @HeliumStmt markup before the line
 # CAUTION I hard coded the line number!
 sed -E -e "892 i\\
-//@HeliumStmt" -i "" compress42.bugsig.c # | head -893 | tail -5
+//@HeliumStmt" compress42.bugsig.c -i # | head -893 | tail -5
 
 # remove a conditional compilation
-sed -E -e "1268,1270 s/^(.*)$/\/\/ \1/" -i "" compress42.bugsig.c
-sed -E -e "1272 s/^(.*)$/\/\/ \1/" -i "" compress42.bugsig.c
+sed -E -e "1268,1270 s/^(.*)$/\/\/ \1/" compress42.bugsig.c -i
+sed -E -e "1272 s/^(.*)$/\/\/ \1/" compress42.bugsig.c -i
 # remove a callsite of comprexx
-sed -E -e "1311 s/^(.*)$/\/\/ \1/" -i "" compress42.bugsig.c
+sed -E -e "1311 s/^(.*)$/\/\/ \1/" compress42.bugsig.c -i
